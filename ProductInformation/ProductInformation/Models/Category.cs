5f6fa2274f000ed8d5dba6187bbf6b261a38b58d
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-04 10:04:38 -0700

    In class practice 1 solution;

---
+++ ProductInformation/ProductInformation/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductInformation.Models
+{
+    [Table("category"]
+
+    public class Category
+    {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(40)")]
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ ProductInformation/ProductInformation/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductInformation.Models
+{
+    [Table("product"]
+    public class Product
+    {
+
+        public Product()
+        {
+            // FK Category 
+        }
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(40)")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "int(10)")]
+        public int CategoryID { get; set; }
+
+
+
+    }
+}
