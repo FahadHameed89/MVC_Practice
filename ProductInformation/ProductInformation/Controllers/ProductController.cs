e6848066d2ea6191939a520152f97d2fb705e7b2
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-04 14:21:50 -0700

    Detailed solution to Practice 8, empty value validation, int parsing validation, form value persistence - message echoing;

--- ProductInformation/ProductInformation/Controllers/ProductController.cs
+++ ProductInformation/ProductInformation/Controllers/ProductController.cs
@@ -12,14 +12,30 @@ namespace ProductInformation.Controllers
 {
     public class ProductController : Controller
     {
+        
 
         // Actions
         public IActionResult Index()
         {
             return RedirectToAction("List");
         }
-        public IActionResult Create()
+        public IActionResult Create(string categoryID, string name)
         {
+            if (Request.Method == "POST")
+                {
+                    try
+                    {
+                        CreateProduct(categoryID, name);
+                    ViewBag.Message = $"Successfully Created Product {name}"!;
+                    }
+                    catch (Exception e)
+                    {
+                        ViewBag.CategoryID = 
+                        ViewBag.Message = "Error: " + e.Message;
+                        ViewBag.Error = true;
+                    }
+                }
+
             return View();
         }
         public IActionResult List()
@@ -27,6 +43,8 @@ namespace ProductInformation.Controllers
             ViewBag.Products = GetProducts();
             return View();
         }
+
+      
 
         // Data Methods
         public List<Product> GetProducts()
@@ -37,6 +55,46 @@ namespace ProductInformation.Controllers
                 results = context.Products.Include(x => x.Category).ToList();
             }
             return results;
+        }
+
+        public void CreateProduct(string categoryID, string name)
+        {
+            int parsedCategoryID;
+
+            // Trimming category id
+            categoryID = categoryID != null ? categoryID.Trim() : null;
+            name = name != null ? name.Trim() : null;
+
+            // Check for individual validation cases and throw an exception if they fail - more details tomorrow
+
+            if (string.IsNullOrWhiteSpace(categoryID))
+            {
+                throw new Exception("Category ID not provided!");
+            }
+            if (!int.TryParse(categoryID, out parsedCategoryID))
+            {
+                throw new Exception("Category ID fails Parse!");
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("Name not provided!");
+            }
+
+            using (ProductInfoContext context = new ProductInfoContext())
+            {
+                if (context.Categories.Where(x => x.ID == parsedCategoryID).Count() < 1)
+                    {
+                    throw new Exception("Category does not exist!");
+                    }
+
+                context.Products.Add(new Product()
+                {
+                    CategoryID = int.Parse(categoryID),
+                    Name = name
+                });
+                context.SaveChanges();
+            }
         }
     }
 }
