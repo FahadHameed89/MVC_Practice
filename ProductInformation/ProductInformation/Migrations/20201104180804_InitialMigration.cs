d96e35345c1471610f61ed30bcb21aefee253645
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-04 11:11:39 -0700

    Completed in class practice 4;

---
+++ ProductInformation/ProductInformation/Migrations/20201104180804_InitialMigration.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductInformation.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int(10)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(40)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int(10)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(40)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_general_ci"),
+                    CategoryID = table.Column<int>(type: "int(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Product_Category",
+                        column: x => x.CategoryID,
+                        principalTable: "category",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "category",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { -1, "Weapons" },
+                    { -2, "Armor" },
+                    { -3, "Materials" },
+                    { -4, "Consumables" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "ID", "CategoryID", "Name" },
+                values: new object[] { -1, -1, "Test" });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "ID", "CategoryID", "Name" },
+                values: new object[] { -2, -2, "Chocolate" });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "ID", "CategoryID", "Name" },
+                values: new object[] { -3, -3, "Beskar" });
+
+            migrationBuilder.CreateIndex(
+                name: "FK_Product_Category",
+                table: "product",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product");
+
+            migrationBuilder.DropTable(
+                name: "category");
+        }
+    }
+}
--- ProductInformation/ProductInformation/Models/ProductInfoContext.cs
+++ ProductInformation/ProductInformation/Models/ProductInfoContext.cs
@@ -37,7 +37,28 @@ namespace ProductInformation.Models
                 .HasCharSet("utf8mb4")
                 .HasCollation("utf8mb4_general_ci");
 
-
+                entity.HasData(
+                   new Category()
+                   {
+                       ID = -1,
+                       Name = "Weapons"
+                   },
+                   new Category()
+                   {
+                       ID = -2,
+                       Name = "Armor"
+                   },
+                   new Category()
+                   {
+                       ID = -3,
+                       Name = "Materials"
+                   },
+                    new Category()
+                    {
+                        ID = -4,
+                        Name = "Consumables"
+                    }
+                );
             });
 
             modelBuilder.Entity<Product>(entity =>
@@ -62,18 +83,28 @@ namespace ProductInformation.Models
                    new Product()
                    {
                        ID = -1,
-                       Name = "Test"
+                       Name = "Test",
+                       CategoryID = -1
                    },
                    new Product()
                    {
                        ID = -2,
-                       Name = "Chocolate"
+                       Name = "Chocolate",
+                       CategoryID = -2
+
                    },
                    new Product()
                    {
                        ID = -3,
-                       Name = "Beskar"
-                   }
+                       Name = "Beskar",
+                       CategoryID = -3
+                   },
+                    new Product()
+                    {
+                        ID = -4,
+                        Name = "Staff of Light",
+                        CategoryID = -3
+                    }
                );
 
 
